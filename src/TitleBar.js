1712bfdb1b77f461e3f14f247c6fd542adfb48b0
Shane <steiner5676@gmail.com>
2021-11-27 15:43:06 -0800

    Working on title bar
    Designed the button look

--- src/TitleBar.js
+++ src/TitleBar.js
@@ -1,14 +1,16 @@
 import './TitleBar.css';
 import exit from './exit.svg';
+import maximize from './maximize.svg';
+import minimize from './minimize.svg';
 
 function TitleBar() {
   return (
-    <div className="TitleBar drag">
-        <div id="BarP1"></div>
-        <div id="BarP2"></div>
-        <div className="titleBarButton" id="BarMin"></div>
-        <div className="titleBarButton" id="BarMax"></div>
-        <div className="titleBarButton" id="BarExit"><img src={exit} className="Icon filter-green" alt="exit" /></div>
+    <div className="TitleBar ">
+        <div className="drag" id="BarP1"></div>
+        <div className="drag" id="BarP2"></div>
+        <div className="titleBarButton" id="BarMin"><img src={minimize} className="Icon filter-blue" alt="min" /></div>
+        <div className="titleBarButton" id="BarMax"><img src={maximize} className="Icon filter-blue" alt="max" /></div>
+        <div className="titleBarButton" id="BarExit"><img src={exit} className="Icon filter-blue" alt="exit" /></div>
     </div>
   );
 }
